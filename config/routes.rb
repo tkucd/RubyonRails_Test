abb56799c7c5df8b11e072e5856fe397e4e79250
tkucd <92442298+tkucd@users.noreply.github.com>
2023-10-04 23:59:21 +0900

    7.2まで完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  resources :articles
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
